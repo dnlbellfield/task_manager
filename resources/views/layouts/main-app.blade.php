fffdf16e84f5dc3fc71e3914d568058a04b63011
Daniel Bellfield <dnlbellfield@gmail.com>
2023-05-19 15:34:03 -0700

    Added "/"  to css/js assets for main-app file

--- resources/views/layouts/main-app.blade.php
+++ resources/views/layouts/main-app.blade.php
@@ -8,9 +8,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.cdnfonts.com/css/general-sans?styles=135312,135310,135313,135303" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/tailwind/tailwind.min.css">
-  <link rel="icon" type="image/png" sizes="32x32" href="shuffle-for-tailwind.png">
-  <script src="js/main.js"></script>
+  <link rel="stylesheet" href="/css/tailwind/tailwind.min.css">
+  <link rel="icon" type="image/png" sizes="32x32" href="/shuffle-for-tailwind.png">
+  <script src="/js/main.js"></script>
 </head>
 
 <body class="antialiased bg-body text-body font-body">
